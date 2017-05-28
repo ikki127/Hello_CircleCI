383c787b07d168ada109087797df0365227fd287
ikki127 <imuchaneta@gmail.com>
2017-05-28 18:28:21 +0200

    Testing a build error

--- src/HelloCircleCI.java
+++ src/HelloCircleCI.java
@@ -3,7 +3,7 @@ public class HelloCircleCI {
 
   public static void main(final String[] args) {
     System.out.println("Hello CircleCI");
-    System.out.println("Ending Program...");
+    System.out.println("Ending Program...")
     // TODO Auto-generated method stub
     //throw new UnsupportedOperationException("Method not yet implemented");
   }
