732b6d09e10e7fe2146d5f08053ac822950aa93f
ikki127 <imuchaneta@gmail.com>
2017-05-28 18:30:48 +0200

    Another Error

--- src/HelloCircleCI.java
+++ src/HelloCircleCI.java
@@ -4,6 +4,7 @@ public class HelloCircleCI {
   public static void main(final String[] args) {
     System.out.println("Hello CircleCI");
     System.out.println("Ending Program...")
+    final tell me
     // TODO Auto-generated method stub
     //throw new UnsupportedOperationException("Method not yet implemented");
   }
